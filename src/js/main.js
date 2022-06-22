4108ec1b61f919f70fbfea8159a4aee4065530ae
valmlord <korshukovstepan@gmail.com>
2022-06-22 10:28:24 +0300

    complete desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,3 @@
-// 'use strict';
-// import  tabs from './modules/tabs';
-// import  modal from './modules/modal';
-// import  timer from './modules/timer';
-// import  cards from './modules/cards';
-// import  calc from './modules/calculator';
-// import  forms from './modules/forms';
-// import  slider from './modules/slider';
-// import {openModal} from './modules/modal';
+ 'use strict';
 
-// window.addEventListener('DOMContentLoaded', function () {
-
-//     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
-
-//     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-//     modal('[data-modal]', '.modal', modalTimerId);
-//     timer('.timer', '2022-06-09');
-//     cards();
-//     calc();
-//     forms('form', modalTimerId);
-//     slider({
-//         container : '.offer__slider',
-//         nextArrow : '.offer__slider-next',
-//         prevArrow : '.offer__slider-prev',
-//         slide : '.offer__slide',
-//         totalCounter : '#total', 
-//         currentCounter : '#current',
-//         wrapper : '.offer__slider-wrapper',
-//         field : '.offer__slider-inner'
-//     });
-
-// });
+import './modules/slider';
